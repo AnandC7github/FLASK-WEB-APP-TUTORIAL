258a456aced53e82950974e28aa7fd2b0a63909c
Anand.C <anandchannazhi01@gmail.com>
2024-01-25 12:34:16 +0530

    changes made in render. changed the route.

--- website/auth.py
+++ website/auth.py
@@ -9,8 +9,8 @@ def login():
 
 @auth.route("/logout")
 def logout():
-    return "<p>Log Out<p>"
+    return "<p>Log Out</p>"
 
 @auth.route("/sign-up")
 def sign_up():
-    return "<p>Sign Up<p>"
+    return "<p>Sign Up</p>"
--- website/views.py
+++ website/views.py
@@ -1,7 +1,9 @@
-from flask import Blueprint
+# views.py
+from flask import Blueprint, render_template
+from flask_login import current_user  # Import current_user from Flask-Login
 
-views = Blueprint('views',__name__)
+views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html", user=current_user)
